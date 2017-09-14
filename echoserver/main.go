72acbf215034f401243c0532f1c7ed742dbd0d46
Chad Ostler <chad.ostler@vivint.com>
2017-09-13 22:55:45 -0600

    initial relay

---
+++ echoserver/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"time"
+)
+
+var (
+	host = flag.String("h", "localhost", "host")
+	port = flag.Int("p", 8080, "port")
+)
+
+func main() {
+	addr := fmt.Sprintf("%v:%v", *host, *port)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+
+	publicAddr := []byte{}
+	buf := make([]byte, 100)
+
+outer:
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			panic(err)
+		}
+		if n > 0 {
+			for i := 0; i < n; i++ {
+				if buf[i] == '\n' {
+					publicAddr = append(publicAddr, buf[:i]...)
+					break outer
+				}
+			}
+			publicAddr = append(publicAddr, buf...)
+		} else {
+			time.Sleep(time.Second / 2)
+		}
+	}
+
+	fmt.Printf("established relay address: %s\n", publicAddr)
+
+	_, err = io.Copy(os.Stdout, conn)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"sync"
+	"syscall"
+)
+
+var (
+	host = flag.String("h", "localhost", "host")
+	port = flag.Int("p", 8080, "port")
+)
+
+type Server struct {
+	host string
+	port int
+
+	portmu         sync.Mutex
+	relayPortStart int
+}
+
+func (s *Server) Serve() error {
+	listener, err := net.Listen("tcp", fmt.Sprintf("%v:%v", s.host, s.port))
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		go func(conn net.Conn) {
+			fmt.Println("New relay server connection", conn.RemoteAddr())
+			p := s.NextPort()
+			addr := fmt.Sprintf("%v:%v", s.host, p)
+			l, err := net.Listen("tcp", addr) // TODO: some resiliency around finding an address to connect to
+			if err != nil {
+				fmt.Println("failed to listen to addr", addr, err)
+				return
+			}
+
+			_, err = conn.Write([]byte(addr + "\n"))
+			if err != nil {
+				fmt.Println("Failed to send relay host", err)
+				_ = conn.Close()
+			}
+
+			r := &relay{
+				host:     s.host,
+				port:     p,
+				listener: l,
+				client:   conn,
+			}
+			go func() {
+				err = r.Serve()
+				if err != nil {
+					fmt.Printf("serve failed%v\n\n%#v\n", err, r)
+				}
+				fmt.Println("Finished client serving")
+			}()
+		}(conn)
+	}
+}
+
+func (s *Server) NextPort() int {
+	s.portmu.Lock()
+	if s.relayPortStart == 0 {
+		s.relayPortStart = *port
+	}
+
+	s.relayPortStart++
+	defer s.portmu.Unlock()
+	return s.relayPortStart
+}
+
+type relay struct {
+	host string
+	port int
+
+	listener net.Listener
+	client   net.Conn
+}
+
+func (r *relay) Serve() error {
+	defer r.listener.Close()
+	for {
+		conn, err := r.listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		go func(conn net.Conn) {
+			fmt.Println("New relay client connection", conn.RemoteAddr())
+			_, err = io.Copy(r.client, conn)
+			if err != nil {
+				fmt.Println("Closing relay client connection", conn.RemoteAddr(), err)
+			}
+			fmt.Println("Closing relay client connection", conn.RemoteAddr())
+			conn.Close()
+		}(conn)
+	}
+}
+
+func main() {
+	s := &Server{
+		host: *host,
+		port: *port,
+	}
+	err := s.Serve()
+	if err != nil {
+		panic(err)
+	}
+}
